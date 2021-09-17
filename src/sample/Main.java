dca5212ab00cefda8682228889db54bc8e47813e
Silviya <silviyasamrin@gmail.com>
2021-09-18 00:39:05 +0600

    initial project setup complete

---
+++ src/sample/Main.java
@@ -0,0 +1,49 @@
+package sample;
+
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
+        import javafx.scene.Parent;
+        import java.io.IOException;
+
+
+public class Main extends Application {
+    public static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("AAAAAAAAAAAATTTTEEEENNNNN!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args)  {
+        launch();
+    }
+
+//    public static void main(String[] args) throws SQLException {
+//        try {
+//            String url = "jdbc:mysql://localhost:3306/at_managment";
+//            String username = "root";
+//            String password = "14101999";
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection(url, username, password);
+//            Statement statement = connection.createStatement();
+//            String connectQuery = "SELECT * FROM courses";
+//
+//            ResultSet queryOutput = statement.executeQuery(connectQuery);
+//
+//            while (queryOutput.next()) {
+//                System.out.println(queryOutput.getString("name"));
+//            }
+////            launch();
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            throw new IllegalStateException("Cannot connect the database! :|");
+//        }
+//    }
+}
---
+++ src/sample/MainController.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import org.w3c.dom.events.MouseEvent;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private Button create_class_btn;
+    @FXML
+    private Pane cse_2201;
+
+    @FXML
+    void onCreateClass(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(getClass().getResource("create-course.fxml"));
+        Scene scene = new Scene((Parent) root, 600, 480);
+        Main.primaryStage.setScene(scene);
+    }
+
+    @FXML
+    public void onCourseOpen(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+//        Object root = FXMLLoader.load(getClass().getResource("course.fxml"));
+//        Scene scene = new Scene((Parent) root, 900, 600);
+//        Main.primaryStage.setScene(scene);
+    }
+    @FXML
+    public void onCse(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene( root, 900, 600);
+        Main.primaryStage.setScene(scene);
+    }
+}
