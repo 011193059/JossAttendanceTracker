c0e4565b42615314d9d086d273cbe2750e4ed552
Silviya <silviyasamrin@gmail.com>
2021-09-24 01:08:21 +0600

    ui design korsi

---
+++ src/Controllers/AdminHome.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class AdminHome {
+    public void onAdminCse1(MouseEvent mouseEvent) {
+    }
+
+    public void onAdminCse2(MouseEvent mouseEvent) {
+    }
+
+    public void onAdminCse3(MouseEvent mouseEvent) {
+    }
+
+    public void onNewStudent(ActionEvent actionEvent) {
+    }
+
+    public void onNewTeacher(ActionEvent actionEvent) {
+    }
+
+    public void onCourseOpen(ActionEvent actionEvent) {
+    }
+}
---
+++ src/Controllers/AdminNewTeacher.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+
+public class AdminNewTeacher {
+    public void onSaveTeacher(ActionEvent actionEvent) {
+    }
+
+    public void onCancelTeacher(ActionEvent actionEvent) {
+    }
+}
